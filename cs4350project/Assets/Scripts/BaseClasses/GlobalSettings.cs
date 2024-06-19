f664f3a2f636fd4ad4106cd0d70888720e0a57fd
zrei <88964793+zrei@users.noreply.github.com>
2024-06-19 22:56:17 +0800

    Complete draft save manager

--- cs4350project/Assets/Scripts/BaseClasses/GlobalSettings.cs
+++ cs4350project/Assets/Scripts/BaseClasses/GlobalSettings.cs
@@ -5,4 +5,10 @@ public class GlobalSettings : Singleton<GlobalSettings>
     [Header("Player Settings")]
     [SerializeField] private float m_PlayerVelocity;
     public static float PlayerVelocity => Instance.m_PlayerVelocity;
+    [Header("Starting Gameplay Values")]
+    [SerializeField] private float m_StartingStarvationValue = 10.0f;
+    public static float StartingStarvationValue => Instance.m_StartingStarvationValue;
+    [Header("Starting Config Values")]
+    [SerializeField] private float m_StartingVolume = 100.0f;
+    public static float StartingVolume => Instance.m_StartingVolume;
 }
