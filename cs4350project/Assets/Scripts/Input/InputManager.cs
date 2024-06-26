10a2c5f1921a27777ff5c7bd76dcbe746735af47
zrei <88964793+zrei@users.noreply.github.com>
2024-06-26 08:58:18 +0800

    Set up input manager

--- cs4350project/Assets/Scripts/Input/InputManager.cs
+++ cs4350project/Assets/Scripts/Input/InputManager.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
 
+public enum InputTypes
+{
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    MOVE_DOWN,
+    MOVE_UP,
+    INTERACT,
+    UI_CLICK, // need UI_SELECT?
+    UI_MOVE_UP,
+    UI_MOVE_DOWN,
+    UI_MOVE_LEFT,
+    UI_MOVE_RIGHT,
+    UI_CLOSE,
+    UI_PAUSE
+}
+
+// i think this conflicts with unity's in-built stuff
+// also might need to be a class to allow data changes
+public struct Input 
+{
+    public readonly InputTypes InputType;
+    public bool IsBlocked;
+    public bool IsActive;
+
+    public Input(InputTypes inputType, bool isBlocked)
+    {
+        InputType = inputType;
+        IsBlocked = isBlocked;
+        IsActive = false;
+    }
+}
+
 public class InputManager : Singleton<InputManager>
 {
+
+    private Dictionary<InputTypes, Input> Inputs;;   
+    // another dictionary to map THE ACTUAL INPUT KEY to the input? or just put the input key down or something
+    // also need a is held possibly. maybe. probably?
     // subscribe to events and handle dependencies here
     protected override void HandleAwake()
     {
-
+        base.HandleAwake();
     }
 
     // unsubscribe to events and cleanup
     protected override void HandleDestroy()
     {
-        
+        base.HandleDestroy();
+    }
+
+    public bool IsInputActive()
+    {
+
     }
 }
