71ae887e9526a8d238eadeaf4f22901bb5fa34a7
zrei <88964793+zrei@users.noreply.github.com>
2024-07-04 15:23:21 +0800

    UI layer + manager

--- cs4350project/Assets/Scripts/UI/UIManager.cs
+++ cs4350project/Assets/Scripts/UI/UIManager.cs
@@ -1,15 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : Singleton<UIManager>
 {
+    private Stack<UILayer> m_OpenLayers;
+    private Canvas m_UICanvas;
+
     // subscribe to events and handle dependencies here
     protected override void HandleAwake()
     {
-
+        m_OpenLayers = new Stack<UILayer>();
+        base.HandleAwake();
     }
 
     // unsubscribe to events and cleanup
     protected override void HandleDestroy()
+    {
+        
+    }
+
+    /// <summary>
+    /// Instantiates the game object prefab and opens its UI layer
+    /// </summary>
+    public void OpenLayer(GameObject gameObject)
+    {
+        
+    }
+
+    public void CloseLayer()
     {
         
     }
