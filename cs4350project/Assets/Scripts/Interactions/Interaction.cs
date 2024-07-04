5bd6bc512a8af112d13fded9d4ea1571f4b0c2d1
zrei <88964793+zrei@users.noreply.github.com>
2024-07-04 15:17:54 +0800

    Expand interaction

--- cs4350project/Assets/Scripts/Interactions/Interaction.cs
+++ cs4350project/Assets/Scripts/Interactions/Interaction.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 public abstract class Interaction : MonoBehaviour
 {
     [SerializeField] private Collider2D m_InteractionCollider;
-    [SerializeField] private bool m_RequireHold; // needed? Idk i'll just add it first
 
+    [Header("Hold Interaction")]
+    [SerializeField] private bool m_RequireHold; // needed? Idk i'll just add it first
+    [SerializeField] private float m_HoldDuration;
+
+    // can also add a bunch of events + conditions under which it is disabled...
+    // also add the indicator popup if necessary
     private void OnTriggerEnter()
     {
-        //
+        //InputManager.Instance.SubscribeToAction(InputType.PLAYER_INTERACTION, HandleInteraction);
     }
 
     private void OnTriggerExit()
     {
-
+        //InputManager.Instance.UnsubscribeToAction(InputType.PLAYER_INTERACTION, HandleInteraction);
     }
 
-    private void Awake()
+    private void ToggleEnabled(bool isEnabled)
     {
-
+        m_InteractionCollider.enabled = isEnabled;
+        //InputManager.Instance.UnsubscribeToAction(InputType.PLAYER_INTERACTION, HandleInteraction);
     }
 
     protected abstract void HandleInteraction();
