9fadcec2a9fdbec0123de73a6953c25a8c42e9d9
zrei <88964793+zrei@users.noreply.github.com>
2024-07-08 08:58:58 +0800

    Virtual cam scaffolding

---
+++ cs4350project/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Cinemachine;
+
+// hm...
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private Cinemachine.VirtualCamera virtualCam;
+
+    public void SetWorldPosition(Vector3 worldPosition)
+    {
+        virtualCam.transform.position = worldPosition;
+    }
+
+    public void SetWorldPosition(Vector2 worldPosition)
+    {
+        float zPos = virtualCam.transform.position.z;
+        virtualCam.transform.position = new Vector3(worldPosition.x, worldPosition.y, zPos);
+    }
+
+    public void SetFollow(Transform toFollow, bool hardSet = false)
+    {
+        virtualCam.Follow = toFollow;
+
+        if (hardSet)
+            SetWorldPosition(toFollow.position);
+    }
+}
