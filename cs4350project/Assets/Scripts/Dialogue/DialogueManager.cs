0d8f23d8400d2eafe034d3f2a42817e445675091
zrei <88964793+zrei@users.noreply.github.com>
2024-07-25 22:05:40 +0800

    Animated dialogue

--- cs4350project/Assets/Scripts/Dialogue/DialogueManager.cs
+++ cs4350project/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 // uhh maybe make this NOT a singleton later but you can leave it be for now
 public class DialogueManager : Singleton<DialogueManager>
 {
-    [SerializeField] private GameObject m_DialogueDisplay;
+    [SerializeField] private UILayer m_DialogueDisplay;
 
     private UI_Dialogue m_DialogueInstance;
 
+    private bool m_Reached = false;
     public void DisplayDialogue()
     {
         m_DialogueInstance = (UI_Dialogue) UIManager.Instance.OpenLayer(m_DialogueDisplay);
+        m_DialogueInstance.Initialise(OnReachEndOfLine);
         m_DialogueInstance.SetDialogueLine(true, "Hello", "HELLLLLLLLO!!!!!!!", null);
+    }
+
+    private void OnReachEndOfLine()
+    {
+        Debug.Log("HEH!");
+        if (!m_Reached)
+            m_DialogueInstance.SetDialogueLine(true, "Owo", "Yayyyy", null);
+
+        m_Reached = true;
     }
 }
--- cs4350project/Assets/Scripts/UI/Dialogue/UI_Dialogue.cs
+++ cs4350project/Assets/Scripts/UI/Dialogue/UI_Dialogue.cs
@@ -4,41 +4,91 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using DG.Tweening;
-
+using System;
+using UnityEngine.InputSystem;
 public class UI_Dialogue : UILayer
 {
     // would it be faster to move the sprite around while rotating it or just have two
     // we DO need an input blocker
     // zzzz remember
 
+    // things to respond to: 
+    // skip to end of line
+    // skip to next line
+    // hmmm. it will know if it's in the middle of a line, that is knowledge it should hold
+    // it WILL NOT store the lines itself that will be stored by?
+
+    // or i suppose the entire dialogue SO could be passed here in all honesty
+    // dk that part can still change
+    [Header("UI References")]
     [SerializeField] private Image m_LeftSprite;
     [SerializeField] private Image m_RightSprite;
     [SerializeField] private TextMeshProUGUI m_DialogueText;
     [SerializeField] private TextMeshProUGUI m_CharacterName;
     [SerializeField] private GameObject m_IndicatorArrow;
 
+    [Header("Values")]
+    [SerializeField] private float m_PerCharacterTime = 0.1f;
+
     private Coroutine m_DialogueCoroutine;
+    private bool m_AnimatingCurrLine;
+
+    private Action m_OnReachEndOfLine;
+
+    private string m_CurrLine;
 
     public override void HandleClose()
     {
-        
+        InputManager.Instance.UnsubscribeToAction(InputType.UI_SELECT, HandleSelect);
     }
 
     public override void HandleOpen()
     {
-        
+        InputManager.Instance.SubscribeToAction(InputType.UI_SELECT, HandleSelect);
+    }
+
+    private void HandleSelect(InputAction.CallbackContext _)
+    {
+        if (m_AnimatingCurrLine)
+            CompleteLine();
+        else
+            m_OnReachEndOfLine?.Invoke();
+    }
+
+    public void Initialise(Action onReachEndOfLine)
+    {
+        m_OnReachEndOfLine = onReachEndOfLine;
     }
 
     // give the dialogue. hm.
-    public void SetDialogueLine(bool isLeftSide, string characterName, string line, Sprite characterSprite)
+    public void SetDialogueLine(bool isLeftSide, string characterName, string line, Sprite characterSprite, bool instant = false)
     {
-        m_DialogueCoroutine = StartCoroutine(AnimateDialogueLine(line, line.Length));
+        m_CurrLine = line;
+
+        m_CharacterName.text = characterName;
+        if (isLeftSide)
+        {
+            m_LeftSprite.sprite = characterSprite;
+        } else
+        {
+            m_RightSprite.sprite = characterSprite;
+        }
+        m_LeftSprite.gameObject.SetActive(isLeftSide);
+        m_RightSprite.gameObject.SetActive(!isLeftSide);
+
+        ClearDialogueLine();
+
+        if (instant)
+            CompleteLine();
+        else
+            m_DialogueCoroutine = StartCoroutine(AnimateDialogueLine(line, line.Length));
     }
 
     private IEnumerator AnimateDialogueLine(string line, int numChars)
     {
+        m_AnimatingCurrLine = true;
+        
         int index = 0;
-        float timePerChar = 17 / numChars;
         m_DialogueText.text = string.Empty;
 
         while (index < numChars)
@@ -46,9 +96,11 @@ public class UI_Dialogue : UILayer
             Debug.Log("A line!");
             m_DialogueText.text += line[index];
             ++index;
-            yield return new WaitForSecondsRealtime(timePerChar);
+            yield return new WaitForSecondsRealtime(m_PerCharacterTime);
         }
-        m_IndicatorArrow.SetActive(true);
+
+        m_DialogueCoroutine = null;
+        CompleteLine();
     }
 
     private void ClearDialogueLine()
@@ -57,16 +109,16 @@ public class UI_Dialogue : UILayer
         m_IndicatorArrow.SetActive(false);
     }
 
-    private void OnClickNext()
+    private void CompleteLine()
     {
         if (m_DialogueCoroutine != null)
         {
-            // complete dialogue line
+            StopCoroutine(m_DialogueCoroutine);
+            m_DialogueCoroutine = null;
+        }
 
-        }
-        else
-        {
-            // skip to next dialogue line or end the dialogue
-        }
+        m_AnimatingCurrLine = false;
+        m_DialogueText.text = m_CurrLine;
+        m_IndicatorArrow.SetActive(true);
     }
 }
