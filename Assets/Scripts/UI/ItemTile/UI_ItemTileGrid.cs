a8346030b3e49cb7ed66a340156c61d16583c7c1
zrei <88964793+zrei@users.noreply.github.com>
2025-08-13 22:17:29 +0800

    item tiles

---
+++ Assets/Scripts/UI/ItemTile/UI_ItemTileGrid.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class UI_ItemTileGrid : MonoBehaviour
+{
+    [SerializeField] private UI_ItemTile m_ItemTile;
+
+    // store the grid sizer here
+
+    private List<UI_ItemTile> m_ItemTileObjs;
+}
